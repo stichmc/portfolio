4304b1c28abcc8c3419ad0cacbeaed8c288a9f35
stichmc <mcstich@outlook.com>
2024-01-23 15:45:58 -0700

    card fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,57 +37,57 @@ const components: { title: string; href: string; description: string }[] = [
 
 export default function Navbar() {
   return (
-    <NavigationMenu className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link to="/about-me">
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            About Me
-          </NavigationMenuLink>
-        </Link>
-        </NavigationMenuItem>
+      <NavigationMenu className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link to="/about-me">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              About Me
+            </NavigationMenuLink>
+          </Link>
+          </NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <Link to="/contact-info">
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            Contact Info
-          </NavigationMenuLink>
-        </Link>
-        </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/contact-info">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Contact Info
+            </NavigationMenuLink>
+          </Link>
+          </NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <Link
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    to="/projects/synergy"
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Projects</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <li className="row-span-3">
+                  <NavigationMenuLink asChild>
+                    <Link
+                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                      to="/projects/synergy"
+                    >
+                      <img className="h-14 w-14" src="/synergy_logo.svg" />
+                      <div className="mb-2 mt-4 text-lg font-medium">Synergy</div>
+                      <p className="text-sm leading-tight text-muted-foreground">
+                        A more trustworthy, reliable, and secure platform for
+                        collaboration and communication.
+                      </p>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
                   >
-                    <img className="h-14 w-14" src="/synergy_logo.svg" />
-                    <div className="mb-2 mt-4 text-lg font-medium">Synergy</div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      A more trustworthy, reliable, and secure platform for
-                      collaboration and communication.
-                    </p>
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
   );
 }
 
--- src/pages/ContactInfo.tsx
+++ src/pages/ContactInfo.tsx
@@ -37,6 +37,9 @@ const formSchema = z.object({
   name: z.string().min(2, {
     message: "Your name must be at least 2 characters.",
   }),
+  email: z.string().min(2, {
+    message: "Your name must be at least 2 characters.",
+  }),
 })
 
 const ContactInfo = () => {
@@ -44,6 +47,7 @@ const ContactInfo = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
+      email: "",
     },
   })
 
@@ -52,7 +56,7 @@ const ContactInfo = () => {
   }
 
   return (
-    <>
+    <div className="grid grid-cols-3">
       <Card className="w-[160px]">
         <CardHeader>
           <CardTitle><img src="./linkedin.svg" className="h-10 w-10"/>LinkedIn</CardTitle>
@@ -68,6 +72,39 @@ const ContactInfo = () => {
         </a>
         </CardContent>
       </Card>
+
+      <Card className="w-[160px]">
+        <CardHeader>
+          <CardTitle><img src="./github.svg" className="h-10 w-10"/>GitHub</CardTitle>
+        </CardHeader>
+        <CardContent>
+        <a
+        href="https://github.com/stichmc"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-gray-950 text-white px-4 py-2 rounded-full text-sm"
+      >
+        View Profile
+        </a>
+        </CardContent>
+      </Card>
+
+      <Card className="w-[160px]">
+        <CardHeader>
+          <CardTitle><img src="./stack-overflow.svg" className="h-10 w-10"/>Stack Overflow</CardTitle>
+        </CardHeader>
+        <CardContent>
+        <a
+        href="https://github.com/stichmc"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-gray-950 text-white px-4 py-2 rounded-full text-sm"
+      >
+        View Profile
+        </a>
+        </CardContent>
+      </Card>
+
       <Card className="w-[350px]">
         <CardHeader>
           <CardTitle>Direct Message</CardTitle>
@@ -75,7 +112,7 @@ const ContactInfo = () => {
         </CardHeader>
         <CardContent>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
               <FormField
                 control={form.control}
                 name="name"
@@ -89,12 +126,25 @@ const ContactInfo = () => {
                   </FormItem>
                 )}
               />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               <Button type="submit" className="rounded-full">Submit</Button>
             </form>
           </Form>
         </CardContent>
       </Card>
-    </>
+    </div>
   )
 }
 
