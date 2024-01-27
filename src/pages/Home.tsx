b395df40ad977486b9ef9d4c9832f03e8b8cec96
stichmc <mcstich@outlook.com>
2024-01-27 09:29:38 -0700

    download button for resume

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,17 +2,30 @@ import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 
 const Home = () => {
+  const handleDownload = () => {
+    const pdfFilePath = "/resume.pdf";
+    const anchor = document.createElement("a");
+    anchor.href = pdfFilePath;
+    anchor.download = "Michael_Stich_Resume.pdf";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  };
+
   return (
     <div className="flex justify-center items-center h-full mt-48">
       <div className="text-black p-8 rounded-md shadow-lg max-w-3xl">
         <h1 className="text-4xl font-bold mb-4">Hi, I'm Michael Stich!</h1>
         <h2 className="text-2xl mb-4">
-          An aspiring software engineering student with 2 years of professional
+          An aspiring software engineering student with 2+ years of professional
           software development experience.
         </h2>
 
         <div className="flex items-center space-x-4 mb-4">
-          <Button className="bg-gray-950 text-white px-4 py-2 rounded-full text-lg transition-transform transform hover:scale-110">
+          <Button
+            onClick={handleDownload}
+            className="bg-gray-950 text-white px-4 py-2 rounded-full text-lg transition-transform transform hover:scale-110"
+          >
             Download Resume
           </Button>
         </div>
