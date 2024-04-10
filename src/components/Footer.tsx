6270b135e87f4bf021dbc5fa54d0562eeaf387bf
stichmc <mcstich@outlook.com>
2024-04-09 22:52:30 -0600

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,41 +1,27 @@
 import { Link } from "react-router-dom";
+import linkedInIcon from "@/assets/footer/linkedin.svg";
+import gitHubIcon from "@/assets/footer/github.svg";
+import stackOverflowIcon from "@/assets/footer/stack-overflow.svg";
+import mailIcon from "@/assets/footer/mail.svg";
 
 const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full bg-black p-4 flex items-center justify-center space-x-4">
-      <Link
-        to="https://www.linkedin.com/in/mcstich?trk=profile-badge"
-        target="_blank"
-      >
-        <img
-          src={`${import.meta.env.BASE_URL}linkedin.svg`}
-          className="h-14 w-14 transition-transform transform hover:scale-125"
-          alt="LinkedIn"
-        />
+      <Link to="https://www.linkedin.com/in/mcstich?trk=profile-badge" target="_blank">
+        <img src={linkedInIcon} className="h-14 w-14 transition-transform transform hover:scale-110" alt="LinkedIn" />
       </Link>
       <Link to="https://github.com/stichmc" target="_blank">
-        <img
-          src={`${import.meta.env.BASE_URL}github.svg`}
-          className="h-12 w-12 transition-transform transform hover:scale-125"
-          alt="GitHub"
-        />
+        <img src={gitHubIcon} className="h-12 w-12 transition-transform transform hover:scale-110" alt="GitHub" />
       </Link>
-      <Link
-        to="https://stackoverflow.com/users/21374669/stichmc"
-        target="_blank"
-      >
+      <Link to="https://stackoverflow.com/users/21374669/stichmc" target="_blank">
         <img
-          src={`${import.meta.env.BASE_URL}stack-overflow.svg`}
-          className="h-14 w-14 transition-transform transform hover:scale-125"
+          src={stackOverflowIcon}
+          className="h-14 w-14 transition-transform transform hover:scale-110"
           alt="Stack Overflow"
         />
       </Link>
       <Link to="mailto:mcstich@outlook.com" target="_blank">
-        <img
-          src={`${import.meta.env.BASE_URL}outlook.svg`}
-          className="h-14 w-14 transition-transform transform hover:scale-125"
-          alt="Outlook"
-        />
+        <img src={mailIcon} className="h-14 w-14 transition-transform transform hover:scale-110" alt="Outlook" />
       </Link>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,6 @@ import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
 
 const Home = () => {
-  // const handleDownload = () => {
-  //   const pdfFilePath = `${import.meta.env.BASE_URL}resume.pdf`;
-  //   const anchor = document.createElement("a");
-  //   anchor.href = pdfFilePath;
-  //   anchor.download = "Michael_Stich_Resume.pdf";
-  //   document.body.appendChild(anchor);
-  //   anchor.click();
-  //   document.body.removeChild(anchor);
-  // };
-
   const [fadeIn, setFadeIn] = useState(false);
 
   useEffect(() => {
@@ -27,14 +17,10 @@ const Home = () => {
       <div className="text-black p-8 rounded-md shadow-lg sm:max-w-3xl">
         <h1 className="text-4xl font-bold mb-4">Hi, I'm Michael Stich!</h1>
         <h2 className="text-2xl mb-4">
-          An aspiring software engineering student with 2+ years of professional
-          software development experience.
+          An aspiring software engineering student with 2+ years of professional software development experience.
         </h2>
 
-        <a
-          href={`${import.meta.env.BASE_URL}Michael_Stich_Resume.pdf`}
-          target="_blank"
-        >
+        <a href={`${import.meta.env.BASE_URL}Michael_Stich_Resume.pdf`} target="_blank">
           <Button
             // onClick={handleDownload}
             className="bg-gray-950 text-white px-4 py-2 rounded-full text-lg transition-transform transform hover:scale-110"
@@ -43,13 +29,6 @@ const Home = () => {
           </Button>
         </a>
       </div>
-
-      {/* <div className="ml-12 hidden xl:block">
-        <img
-          src={`${import.meta.env.BASE_URL}miguel.jpg`}
-          className="h-64 w-64 rounded-full shadow-2xl"
-        />
-      </div> */}
     </div>
   );
 };
