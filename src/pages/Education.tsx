c2aa64e1ff42b3d080538ae424840cef281b1ae5
stichmc <mcstich@outlook.com>
2024-04-10 13:13:26 -0600

    mobile ui adjustments

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -11,7 +11,7 @@ const Education = () => {
       <div className="bg-gray-200 h-[95vh]">
         <FadeIn>
           <div className="flex items-center justify-center lg:p-24 p-8 h-[80vh]">
-            <div className="flex flex-row md:space-x-8 w-full h-[36vh] items-center justify-center mt-20">
+            <div className="flex flex-row md:space-x-8 w-full h-[36vh] items-center justify-center mt-2">
               <div className="text-black p-8 rounded-2xl shadow-lg w-full bg-white flex flex-row space-x-4 xl:space-x-48">
                 <div className="flex flex-col justify-center">
                   <h1 className="text-3xl sm:text-4xl font-bold mb-4">Education</h1>
