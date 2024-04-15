da6001a3bac82ae76196d5ed5360786f1b141ef7
stichmc <mcstich@outlook.com>
2024-04-15 14:46:51 -0600

    updated content

--- src/pages/projects/SatTrack.tsx
+++ src/pages/projects/SatTrack.tsx
@@ -24,33 +24,18 @@ const SatTrack = () => {
             </div>
 
             <div className="mt-6 sm:mt-0">
-              <div className="mb-8 mt-8">
+              <div className="mb-8 sm:mt-8">
                 <img src={satTrackPicture} alt="SatTrack" className="rounded-xl" />
               </div>
 
               <h1 className="text-xl sm:text-3xl font-bold mb-4">Your go-to satellite tracking website!</h1>
               <p className="text-sm sm:text-xl mb-8">
-                Sat-Track is designed to provide real-time tracking information for various satellites orbiting Earth,
-                making it easier for enthusiasts, researchers, and curious minds to observe these celestial objects from
-                the comfort of their screens. Whether you're interested in spotting the International Space Station
-                (ISS), the Hubble Space Telescope, or any other satellite, Sat-Track has you covered. My team and I
-                developed this entire project in 24 hours at HackCU 2024.
+                Sat-Track is designed to provide real-time tracking information for satellites orbiting Earth, making it
+                easier for enthusiasts, researchers, and curious minds to observe the telemetry data from satellites.
+                Whether you're interested in spotting the International Space Station (ISS), the Hubble Space Telescope,
+                or any other satellite, Sat-Track has you covered. My team and I developed this entire project within 24
+                hours for the HackCU 2024 hackathon.
               </p>
-
-              <div className="mb-8 items-center md:flex">
-                <div className="md:w-1/2">
-                  <h1 className="text-xl sm:text-2xl font-bold mb-4">Globe:</h1>
-                  <p className="text-sm sm:text-xl mb-8">
-                    This labor of love, this globe rendered by Three.js took a majority of our time. The satellite data
-                    we receive from our external apis give us all the positioning and velocity variables we needed to
-                    approximate a satellites location and orbit around Earth. Our little green dots are able to move
-                    around the globe accurately to that satellite's current position, velocity, and altitude.
-                  </p>
-                </div>
-                <div className="md:w-1/3 md:pr-8 mx-auto">
-                  <img src={globePicture} alt="Globe" className="h-auto rounded-xl" />
-                </div>
-              </div>
 
               <div className="mb-8 items-center md:flex">
                 <div className="md:w-1/2 md:pr-8 mx-auto">
@@ -60,11 +45,26 @@ const SatTrack = () => {
                 <div className="md:w-1/2 mt-4">
                   <h1 className="text-xl sm:text-2xl font-bold mb-4">Control Panel:</h1>
                   <p className="text-sm sm:text-xl mb-8">
-                    The control panel is where you can see the data we are pulling from our external apis. This includes
-                    the satellite's real time longitude, latitude and altitude. If you have a satellite's id, you can
-                    input it into the control panel and add it to our tracked satellites list. This will also allow you
-                    to see the satellite's position on the globe.
+                    The control panel is where you can see the live satellite data being pulled from the external apis.
+                    This includes the satellite's real time longitude, latitude and altitude. If you have a satellite's
+                    id, you can input it into the control panel and add it to the tracked satellites list. This will
+                    also allow you to see the satellite's position on the globe.
                   </p>
+                </div>
+              </div>
+
+              <div className="mb-8 items-center md:flex">
+                <div className="md:w-1/2">
+                  <h1 className="text-xl sm:text-2xl font-bold mb-4">The Globe:</h1>
+                  <p className="text-sm sm:text-xl mb-8">
+                    Projecting the live telemetry data onto a Three.js rendered globe took a majority of our time on
+                    this project. The green dots orbiting the globe are the live positions of the tracked satellites
+                    relative to Earth. The globe is interactive and can be rotated and zoomed in on to see the live data
+                    in more detail.
+                  </p>
+                </div>
+                <div className="md:w-1/3 md:pr-8 mx-auto">
+                  <img src={globePicture} alt="Globe" className="h-auto rounded-xl" />
                 </div>
               </div>
 
@@ -73,8 +73,8 @@ const SatTrack = () => {
                 <ul className="list-disc">
                   <li className="text-sm sm:text-lg">
                     <strong>Default Display:</strong> Sat-Track comes pre-loaded with information about several popular
-                    orbiters in low Earth orbit, including the ISS and the Hubble Space Telescope. This allows users to
-                    quickly access data about these well-known satellites without the need for manual searches.
+                    satellites in low Earth orbit, including the ISS and the Hubble Space Telescope. This allows users
+                    to quickly access data about these well-known satellites without the need for manual searches.
                   </li>
                   <li className="text-sm sm:text-lg">
                     <strong>Custom Satellite Search:</strong> In addition to the default display, Sat-Track offers users
@@ -83,19 +83,21 @@ const SatTrack = () => {
                     Sat-Track's search functionality makes it easy to find and monitor the desired satellite.
                   </li>
                   <li className="text-sm sm:text-lg">
-                    <strong>Real-Time Updates:</strong> Sat-Track provides real-time updates on the positions and
-                    trajectories of tracked satellites, ensuring that users have access to the most accurate and
-                    up-to-date information available. Whether you're planning a stargazing session or conducting
-                    research, you can rely on Sat-Track to provide reliable tracking data whenever you need it.
+                    <strong>Real-Time Updates:</strong> Sat-Track provides real-time updates on satellite telemetry
+                    data, ensuring that users have access to the most accurate and up-to-date information available.
+                    Whether you're planning a stargazing session or conducting research, you can rely on Sat-Track to
+                    provide reliable tracking data whenever you need it.
                   </li>
                 </ul>
               </div>
 
               <div className="technical-details-section mt-4">
-                <h1 className="text-xl sm:text-2xl font-bold mb-4">Technology:</h1>
+                <h1 className="text-xl sm:text-2xl font-bold mb-4">Technical Details:</h1>
                 <ul className="list-disc">
                   <li className="text-sm sm:text-lg">
-                    <strong>Frontend:</strong> The front end of stat-track employs modern react with typescript.
+                    <strong>Frontend:</strong> The frontend of Sat-Track is built using React, a popular JavaScript
+                    library for building user interfaces. React's component-based architecture allows for modular and
+                    scalable development, enhancing the user experience.
                   </li>
                   <li className="text-sm sm:text-lg">
                     <strong>Backend:</strong> A Postgres SQL database was used to store satellite information, such as
