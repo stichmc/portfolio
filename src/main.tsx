ebd5fe610f1e22f4efaa7fc879d3e7d472fb14e7
stichmc <98062337+stichmc@users.noreply.github.com>
2025-10-02 00:25:30 -0600

    only route to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,21 @@
+// External Imports
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import "./index.css";
 
-// Pages
-import Home from "./pages/Home.tsx";
-import Education from "./pages/Education.tsx";
-// import Anello from "./pages/projects/Anello.tsx";
-import Beyond from "./pages/projects/Beyond.tsx";
-import SatTrack from "./pages/projects/SatTrack.tsx";
-import SpeechText from "./pages/projects/SpeechText.tsx";
-import TimeEscapement from "./pages/projects/TimeEscapement.tsx";
-import Doom from "./pages/projects/Doom.tsx";
+// Internal Imports
+import env from "@/env.ts";
+import App from "@/App";
 
+// Even though this is a single page website we still want to ensure the user can't navigate to a link that doesn't exist
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <div className="h-[100svh] bg-gray-200">
-        <Routes>
-          <Route path="/" element={<Navigate to={`${import.meta.env.BASE_URL}home`} />} />
-          <Route path="*" element={<Navigate to={`${import.meta.env.BASE_URL}home`} />} />
-          <Route path={`${import.meta.env.BASE_URL}home`} element={<Home />} />
-          <Route path={`${import.meta.env.BASE_URL}eduction`} element={<Education />} />
-
-          {/* <Route path={`${import.meta.env.BASE_URL}projects/anello`} element={<Anello />} /> */}
-          <Route path={`${import.meta.env.BASE_URL}projects/beyond`} element={<Beyond />} />
-          <Route path={`${import.meta.env.BASE_URL}projects/hackcu-sattrack`} element={<SatTrack />} />
-          <Route path={`${import.meta.env.BASE_URL}projects/speech-to-text`} element={<SpeechText />} />
-          <Route path={`${import.meta.env.BASE_URL}projects/time-escapement`} element={<TimeEscapement />} />
-          <Route path={`${import.meta.env.BASE_URL}projects/doom`} element={<Doom />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<Navigate to={env.BASE_URL} />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
