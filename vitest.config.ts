10a5759662884afe8abbd8f566717cb215b48c17
Michael Stich <98062337+stichmc@users.noreply.github.com>
2025-10-01 22:34:26 -0600

    Add testing! (#24)

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
