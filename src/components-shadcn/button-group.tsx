50889162e65ec390cb776a4f5b61a0c23c6aa946
stichmc <98062337+stichmc@users.noreply.github.com>
2025-10-05 17:51:49 -0600

    ignore for now

--- src/components-shadcn/button-group.tsx
+++ src/components-shadcn/button-group.tsx
@@ -47,6 +47,7 @@ function ButtonGroupText({
   const Comp = asChild ? Slot : "div"
 
   return (
+    // @ts-ignore
     <Comp
       className={cn(
         "bg-muted shadow-xs flex items-center gap-2 rounded-md border px-4 text-sm font-medium [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none",
