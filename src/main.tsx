a4e752e2e79e4877ed3157a18446818f680f4ac1
stichmc <98062337+stichmc@users.noreply.github.com>
2025-10-05 17:50:45 -0600

    move theme

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,22 @@ import "./index.css";
 
 // Internal Imports
 import env from "@/env.ts";
+import { ThemeProvider } from "@/components-shadcn/theme-provider";
+import ThemeToggle from "@/components/ThemeToggle";
+
+// Pages
 import App from "@/App";
 
-// Even though this is a single page website we still want to ensure the user can't navigate to a link that doesn't exist
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="*" element={<Navigate to={env.BASE_URL} />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider storageKey="mcstich-ui-theme" defaultTheme="dark">
+      <ThemeToggle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<Navigate to={env.BASE_URL} />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
