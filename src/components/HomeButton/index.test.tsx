977d9192a89e2038c91cf34ee2309b65174352c1
stichmc <98062337+stichmc@users.noreply.github.com>
2025-10-08 19:10:46 -0600

    test

--- src/components/HomeButton/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { render } from "@testing-library/react";
-import HomeButton from ".";
-
-test("HomeButton", () => {
-  const rendering = render(<HomeButton />);
-  expect(rendering).toMatchSnapshot();
-});
